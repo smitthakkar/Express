9964362ea0829501463249cef841a7321a775800
Sanveg Rane <ssrane2@ncsu.edu>
2019-10-07 13:35:33 -0400

    Added all books end point

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
+const port = 3100;
 
-var allThingsRouter= require('./things.js');
+var allThingsRouter = require('./things.js');
 
-app.use('/things',allThingsRouter);
+app.use('/things', allThingsRouter);
 
-app.get('/getBookId/:id([0-9]{5})',function(req,res){
-res.send('Book number requested : '+middleWare(req.params.id));
+app.get('/', (req, res) => res.send('Welcome to Express!'))
+
+app.get('/getBookId/:id([0-9]{5})', function (req, res) {
+  res.send('Book number requested : ' + middleWare(req.params.id));
 
 });
 
-function middleWare(id){
-  return 'Processing Id:' + id; 
+function middleWare(id) {
+  return 'Processing Id:' + id;
 }
-app.listen(3000,()=>console.log("Hello World!"));
+app.listen(port, () => console.log(`Server listening at port ${port}!`));
